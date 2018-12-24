dde8b6e1d83029fe5f57f2b383924f73152e6e2d
zoba <43417377+zeinabmohammed@users.noreply.github.com>
2018-12-24 16:28:02 +0200

    Update and rename profanrev.py to profanity_checker.py

--- profanity_checker.py
+++ profanity_checker.py
@@ -1,6 +1,7 @@
 #import os
 import urllib
 def read_text():
+	"""open text file and read it"""
 	quates=open("F:\New folder (2)\movie-quotes\movie_quotes\movie_quotes.txt")
 	content=quates.read()
 	print content
@@ -8,6 +9,7 @@ def read_text():
 	check_profanity(content)
 
 def check_profanity(text_to_check):
+	"""check for profanity words in text file"""
 	
 	connection=urllib.urlopen("http://www.wdylike.appspot.com/?q=text_to_check")
 	output=connection.read()
